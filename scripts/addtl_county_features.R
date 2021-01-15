e5713a78a6d644d6635ece2efc9b0250acab7266
asmae-toumi <atoumi.cu@gmail.com>
2021-01-15 09:21:52 -0500

    additional county features

---
+++ scripts/addtl_county_features.R
@@ -0,0 +1,7 @@
+# devtools::install_github("asmae-toumi/MSVI")
+library(MSVI)
+
+county_rankings <- county_health_rankings
+# source: https://www.countyhealthrankings.org/explore-health-rankings/measures-data-sources/2020-measures
+
+colnames(county_health_rankings)
